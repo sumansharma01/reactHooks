8285408bfd8521fe4793d68ecddae7ec0d4a26f4
sumansharma01 <suman.sharma0331@gmail.com>
2021-04-27 10:56:51 +0530

    useState2

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,8 +4,14 @@ function Counter() {
   const [count, setCount] = useState(0);
   return (
     <div>
-      <button onClick={() => setCount((prevValue) => prevValue + 1)}>
-        Count {count}
+      <button onClick={() => setCount((prevValue) => prevValue + 1)}>+</button>
+      {count}
+      <button
+        onClick={() => {
+          setCount((prevValue) => prevValue - 1);
+        }}
+      >
+        -
       </button>
     </div>
   );
